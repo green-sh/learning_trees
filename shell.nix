87f44593a4430ba5578cbc4707ce6c3ad899d9e7
green-sh <35291049+green-sh@users.noreply.github.com>
2023-10-06 10:11:50 +0200

    Update random_forest.py
    rename
    
    test

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-   my-packages = ps: with ps; [
-	numpy
-	pandas
-   ];
+	my-packages = ps: with ps; [
+		numpy
+		pandas
+	];
 in pkgs.mkShell {
-    packages = with pkgs; [
-	(python3.withPackages my-packages)
-    ];
+	packages = with pkgs; [
+		(python3.withPackages my-packages)
+	];
 }
